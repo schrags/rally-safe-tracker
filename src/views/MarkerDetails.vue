92a4d2d4c51ce0c9fa528901eb2e63bbd41a5d5d
Jon Schrage <jon.schrage@gmail.com>
2021-03-17 11:42:29 -0400

    car details

--- src/views/MarkerDetails.vue
+++ src/views/MarkerDetails.vue
@@ -2,11 +2,12 @@
   <div>
     <p>
       <strong>#{{vehicle.VehicleNumber}} {{vehicle.Make}}</strong><br/>
-      {{vehicle.Drv}}<br/>
-      {{vehicle.Nav}}<br/>
+      <img :src="driverImageSrc"/> {{vehicle.Drv}}<br/>
+      <img :src="coDriverImageSrc"/> {{vehicle.Nav}}<br/>
       {{vehicle.CompetitionStatus}}<br/>
       {{vehicle.RacingStatus}}<br/>
-      Battery: {{vehicle.BatteryVoltage}}<br/>
+      Speed: {{vehicle.Speed}}<br/>
+      Battery: {{vehicle.BatteryVoltage}} volts<br/>
       Last Updated {{vehicle.LastUpdatedTime}} minutes ago
     </p>
   </div>
@@ -20,6 +21,12 @@ export default {
  name: "MarkerDetails",
  props: ["vehicle"],
  computed: {
+   driverImageSrc() {
+     return "https://sas-web-app-cdn.azureedge.net/cdn/CountryFlags/24/" + this.vehicle.DrvCountryCode +".png"
+   },
+   coDriverImageSrc() {
+     return "https://sas-web-app-cdn.azureedge.net/cdn/CountryFlags/24/" + this.vehicle.NavCountryCode +".png"
+   }
  }
 };
 </script>
