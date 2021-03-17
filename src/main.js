3797a3c53dec92301caf9d99bec52ed1bb162d17
Jon Schrage <jon.schrage@gmail.com>
2021-03-17 12:25:58 -0400

    google analytics

--- src/main.js
+++ src/main.js
@@ -7,9 +7,14 @@ import { iconsSet as icons } from './assets/icons/icons.js'
 import store from './store'
 import leaflet from 'leaflet'
 import rotatedMarker from 'leaflet-rotatedmarker'
+import VueAnalytics from 'vue-analytics'
 
 Vue.config.performance = true
 Vue.use(CoreuiVue)
+Vue.use(VueAnalytics, {
+  id: 'G-SD30FRCH2W',
+  checkDuplicatedScript: true
+})
 Vue.prototype.$log = console.log.bind(console)
 
 new Vue({
